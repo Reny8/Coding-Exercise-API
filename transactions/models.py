d1e97dde5e805e68c18161c7c3f57541e5784f05
Reny Reyes <renyreyes8@hotmail.com>
2022-06-09 17:17:06 -0400

    Created a model class for the transactions

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Transactions(models.Model):
+    payer = models.CharField(max_length=50)
+    points = models.IntegerField()
+    timestamp = models.DateField()
+    

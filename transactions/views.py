309abe6d78693571d07b4aad82eb0f7a7595a2dd
Reny Reyes <renyreyes8@hotmail.com>
2022-06-09 18:06:21 -0400

    Created a post function + Tested

--- transactions/views.py
+++ transactions/views.py
@@ -4,9 +4,17 @@ from rest_framework import status
 from .models import Transactions
 from .serializers import TransactionsSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def transactions_details(request):
     if request.method == 'GET':
         transactions = Transactions.objects.all()
         serializer = TransactionsSerializer(transactions, many = True)
         return Response(serializer.data, status.HTTP_200_OK)
+    if request.method == 'POST': 
+        serializer = TransactionsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+

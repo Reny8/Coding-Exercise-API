b0e11fc7610a000ff9c988ef8057ea4d7e13218c
Reny Reyes <renyreyes8@hotmail.com>
2022-06-09 17:52:27 -0400

    Created a function to get all transactions + Tested

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Transactions
+from .serializers import TransactionsSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def transactions_details(request):
+    if request.method == 'GET':
+        transactions = Transactions.objects.all()
+        serializer = TransactionsSerializer(transactions, many = True)
+        return Response(serializer.data, status.HTTP_200_OK)

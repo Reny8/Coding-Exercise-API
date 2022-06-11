e4871fc4e32c2e334e32e22da710ec70328423fc
Reny Reyes <renyreyes8@hotmail.com>
2022-06-10 20:33:48 -0400

    Completed the function for spending points + Tested

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.transactions_details)
+    path('', views.transactions_details),
+    path('<int:points>/', views.spend_points)
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -6,15 +6,48 @@ from .serializers import TransactionsSerializer
 
 @api_view(['GET', 'POST'])
 def transactions_details(request):
+    # GETTING ALL THE TRANSACTIONS IN THE DATABASE
     if request.method == 'GET':
-        transactions = Transactions.objects.all()
+        transactions = Transactions.objects.all().order_by("timestamp")
         serializer = TransactionsSerializer(transactions, many = True)
         return Response(serializer.data, status.HTTP_200_OK)
-    if request.method == 'POST': 
+
+    # ADDING A NEW TRANSACTION
+    elif request.method == 'POST': 
         serializer = TransactionsSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status.HTTP_202_ACCEPTED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST) 
 
+@api_view(['PUT'])
+def spend_points(request,points):
+    #SPENDING POINTS
+    if request.method == 'PUT':
+        # ORDERED THE TRANSACTIONS FROM OLDEST POINTS
+        transactions = Transactions.objects.all().order_by("timestamp")
+        for trans in transactions: 
+            trans.points -= points
+            # WHEN POINTS FOR TO NEGATIVE DISPLAY WILL BE 0
+            if trans.points < 0:
+                points = abs(trans.points)
+                trans.points = 0
+                serializer = TransactionsSerializer(trans, data = request.data, partial = True)
+                if serializer.is_valid(raise_exception = True):
+                    serializer.save()
+            elif trans.points >= 0:
+                points = 0
+                serializer = TransactionsSerializer(trans, data = request.data, partial = True)
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+            serializer_all = TransactionsSerializer(transactions, many=True)
+            return Response(serializer_all.data, status.HTTP_202_ACCEPTED)
+            
+           
+                
+
+                
+
+
+    
 
